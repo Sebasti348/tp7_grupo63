fd724ed18fa4a213ca6274eb7fb69683a0eef15a
vilte <nicolasvilte08@gmail.com>
2024-11-13 17:17:50 -0300

    Finalizacion de los daos

---
+++ src/main/java/ar/edu/unju/escmi/dao/IClienteDao.java
@@ -0,0 +1,15 @@
+package ar.edu.unju.escmi.dao;
+
+import java.util.List;
+
+import ar.edu.unju.escmi.entities.Cliente;
+
+public interface IClienteDao {
+	public void guardarCliente(Cliente cliente);
+
+	public void modificarCliente(Cliente cliente);
+
+	public Cliente obtenerClienteId(Long idcliente);
+
+	public List<Cliente> mostrarClientes();
+}
